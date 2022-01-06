d9a120bad5f327f1ab1a8dce6a63316bbded4113
usama <bitf18a534@pucit.edu.pk>
2022-01-06 14:36:52 +0500

    done

--- app/src/main/java/com/example/arrayadapter/MainActivity.java
+++ app/src/main/java/com/example/arrayadapter/MainActivity.java
@@ -11,20 +11,20 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView=findViewById(R.id.listView);
+        ListView listView=findViewById(R.id.listView);
 
-        ArrayList<String> arrayList=new ArrayList<>();
+        ArrayList<String> arrayList=new ArrayList<String>();
         arrayList.add("Infinix");
         arrayList.add("Vivo");
         arrayList.add("Oppo");
 
-        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.activity_main,arrayList);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item,arrayList);
         listView.setAdapter(arrayAdapter);
     }
 }
